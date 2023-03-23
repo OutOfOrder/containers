50e1e79ee40d4a4939ff1561dff9236121532d3a
Edward Rudd <urkle@outoforder.cc>
2023-03-23 13:18:35 -0400

    Switch to github container repository

--- cyrus-imap-tester/build.sh
+++ cyrus-imap-tester/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 DATE=$(date +%Y%m%d)
-docker build . -f Dockerfile -t outoforder/cyrus-imapd-tester:$DATE-00
-docker image tag outoforder/cyrus-imapd-tester:$DATE-00 outoforder/cyrus-imapd-tester:latest
+docker build . -f Dockerfile -t ghcr.io/outoforder/cyrus-imapd-tester:$DATE-00
+docker image tag ghcr.io/outoforder/cyrus-imapd-tester:$DATE-00 ghcr.io/outoforder/cyrus-imapd-tester:latest
 
 
