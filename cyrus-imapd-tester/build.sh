8dc640ed1144fe540983b72eaf4ce07adc279860
Edward Rudd <urkle@outoforder.cc>
2023-05-27 14:44:41 -0400

    add back docker container registry

--- cyrus-imapd-tester/build.sh
+++ cyrus-imapd-tester/build.sh
@@ -3,5 +3,6 @@
 DATE=$(date +%Y%m%d)
 docker build . -f Dockerfile -t ghcr.io/outoforder/cyrus-imapd-tester:$DATE-00
 docker image tag ghcr.io/outoforder/cyrus-imapd-tester:$DATE-00 ghcr.io/outoforder/cyrus-imapd-tester:latest
+docker image tag ghcr.io/outoforder/cyrus-imapd-tester:latest outoforder/cyrus-imapd-tester:$DATE-00
 
 
