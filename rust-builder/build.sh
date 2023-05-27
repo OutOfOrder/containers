fb60ba70fb57dddb5a95f98111a078f8f57f0141
Edward Rudd <urkle@outoforder.cc>
2023-05-27 14:44:02 -0400

    add back docker container registry

--- rust-builder/build.sh
+++ rust-builder/build.sh
@@ -5,3 +5,7 @@ docker build --no-cache . -f Dockerfile -t ghcr.io/outoforder/rust-builder:rocky
 RUSTC_VERSION=$(docker run --rm ghcr.io/outoforder/rust-builder:rocky8-$DATE rustc --version | sed 's/rustc \([[:digit:].]*\).*/\1/g')
 docker image tag ghcr.io/outoforder/rust-builder:rocky8-$DATE ghcr.io/outoforder/rust-builder:rocky8-$RUSTC_VERSION
 docker image tag ghcr.io/outoforder/rust-builder:rocky8-$DATE ghcr.io/outoforder/rust-builder:latest
+docker image tag ghcr.io/outoforder/rust-builder:latest outoforder/rust-builder:rocky8-$DATE
+docker image tag ghcr.io/outoforder/rust-builder:latest outoforder/rust-builder:rocky8-$RUSTC_VERSION
+docker image tag ghcr.io/outoforder/rust-builder:latest outoforder/rust-builder:latest
+
