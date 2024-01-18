054d9f81ef1e83727d39c5a6bf248ada2667f964
Edward Rudd <urkle@outoforder.cc>
2024-01-18 12:54:47 -0500

    update build scripts to use podman

--- cyrus-imapd-tester/build.sh
+++ cyrus-imapd-tester/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 DATE=$(date +%Y%m%d)
-docker build . -f Dockerfile -t ghcr.io/outoforder/cyrus-imapd-tester:$DATE-00
-docker image tag ghcr.io/outoforder/cyrus-imapd-tester:$DATE-00 ghcr.io/outoforder/cyrus-imapd-tester:latest
-docker image tag ghcr.io/outoforder/cyrus-imapd-tester:latest outoforder/cyrus-imapd-tester:$DATE-00
+podman build . -f podmanfile -t ghcr.io/outoforder/cyrus-imapd-tester:$DATE-00
+podman image tag ghcr.io/outoforder/cyrus-imapd-tester:$DATE-00 ghcr.io/outoforder/cyrus-imapd-tester:latest
+podman image tag ghcr.io/outoforder/cyrus-imapd-tester:latest docker.io/outoforder/cyrus-imapd-tester:$DATE-00
+podman image tag ghcr.io/outoforder/cyrus-imapd-tester:latest docker.io/outoforder/cyrus-imapd-tester:latest
 
 
--- rust-builder/build.sh
+++ rust-builder/build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 DATE=$(date +%Y-%m-%d)
-docker build --no-cache . -f Dockerfile -t ghcr.io/outoforder/rust-builder:rocky8-$DATE
-RUSTC_VERSION=$(docker run --rm ghcr.io/outoforder/rust-builder:rocky8-$DATE rustc --version | sed 's/rustc \([[:digit:].]*\).*/\1/g')
-docker image tag ghcr.io/outoforder/rust-builder:rocky8-$DATE ghcr.io/outoforder/rust-builder:rocky8-$RUSTC_VERSION
-docker image tag ghcr.io/outoforder/rust-builder:rocky8-$DATE ghcr.io/outoforder/rust-builder:latest
-docker image tag ghcr.io/outoforder/rust-builder:latest outoforder/rust-builder:rocky8-$DATE
-docker image tag ghcr.io/outoforder/rust-builder:latest outoforder/rust-builder:rocky8-$RUSTC_VERSION
-docker image tag ghcr.io/outoforder/rust-builder:latest outoforder/rust-builder:latest
+podman build --no-cache . -f Dockerfile -t ghcr.io/outoforder/rust-builder:rocky8-$DATE
+RUSTC_VERSION=$(podman run --rm ghcr.io/outoforder/rust-builder:rocky8-$DATE rustc --version | sed 's/rustc \([[:digit:].]*\).*/\1/g')
+podman image tag ghcr.io/outoforder/rust-builder:rocky8-$DATE ghcr.io/outoforder/rust-builder:rocky8-$RUSTC_VERSION
+podman image tag ghcr.io/outoforder/rust-builder:rocky8-$DATE ghcr.io/outoforder/rust-builder:latest
+podman image tag ghcr.io/outoforder/rust-builder:latest docker.io/outoforder/rust-builder:rocky8-$DATE
+podman image tag ghcr.io/outoforder/rust-builder:latest docker.io/outoforder/rust-builder:rocky8-$RUSTC_VERSION
+podman image tag ghcr.io/outoforder/rust-builder:latest docker.io/outoforder/rust-builder:latest
 
