ba92cb0db4084bb9e1ccf7b7ff599d81b5638927
Edward Rudd <urkle@outoforder.cc>
2024-10-18 11:04:47 -0400

    only tag with docker.io tag names

--- rust-builder/build.sh
+++ rust-builder/build.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 DATE=$(date +%Y-%m-%d)
-podman build --no-cache . -f Dockerfile -t ghcr.io/outoforder/rust-builder:rocky8-$DATE
+podman build --no-cache . -f Dockerfile -t docker.io/outoforder/rust-builder:rocky8-$DATE
 RUSTC_VERSION=$(podman run --rm ghcr.io/outoforder/rust-builder:rocky8-$DATE rustc --version | sed 's/rustc \([[:digit:].]*\).*/\1/g')
-podman image tag ghcr.io/outoforder/rust-builder:rocky8-$DATE ghcr.io/outoforder/rust-builder:rocky8-$RUSTC_VERSION
-podman image tag ghcr.io/outoforder/rust-builder:rocky8-$DATE ghcr.io/outoforder/rust-builder:latest
-podman image tag ghcr.io/outoforder/rust-builder:latest docker.io/outoforder/rust-builder:rocky8-$DATE
-podman image tag ghcr.io/outoforder/rust-builder:latest docker.io/outoforder/rust-builder:rocky8-$RUSTC_VERSION
-podman image tag ghcr.io/outoforder/rust-builder:latest docker.io/outoforder/rust-builder:latest
+podman image tag docker.io/outoforder/rust-builder:rocky8-$DATE docker.io/outoforder/rust-builder:rocky8-$RUSTC_VERSION
+podman image tag docker.io/outoforder/rust-builder:rocky8-$DATE docker.io/outoforder/rust-builder:latest
 
