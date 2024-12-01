20ab48d03bc61f9039651552490a13b1f6a1d60b
Edward Rudd <urkle@outoforder.cc>
2024-12-01 14:01:33 -0500

    Fix typo in build.sh

--- rust-builder/build.sh
+++ rust-builder/build.sh
@@ -2,7 +2,7 @@
 
 DATE=$(date +%Y-%m-%d)
 podman build --no-cache . -f Dockerfile -t docker.io/outoforder/rust-builder:rocky8-$DATE
-RUSTC_VERSION=$(podman run --rm ghcr.io/outoforder/rust-builder:rocky8-$DATE rustc --version | sed 's/rustc \([[:digit:].]*\).*/\1/g')
+RUSTC_VERSION=$(podman run --rm docker.io/outoforder/rust-builder:rocky8-$DATE rustc --version | sed 's/rustc \([[:digit:].]*\).*/\1/g')
 podman image tag docker.io/outoforder/rust-builder:rocky8-$DATE docker.io/outoforder/rust-builder:rocky8-$RUSTC_VERSION
 podman image tag docker.io/outoforder/rust-builder:rocky8-$DATE docker.io/outoforder/rust-builder:latest
 
