a4e9ddae273558ed18d6ab9d25b7451ab21acb31
Edward Rudd <urkle@outoforder.cc>
2025-01-22 11:34:04 -0500

    Add rocky9 based build

--- rust-builder/build.sh
+++ rust-builder/build.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
+set -e
+
 DATE=$(date +%Y-%m-%d)
-podman build --no-cache . -f Dockerfile -t docker.io/outoforder/rust-builder:rocky8-$DATE
+podman build --no-cache . -f Containerfile.rocky8 -t docker.io/outoforder/rust-builder:rocky8-$DATE
 RUSTC_VERSION=$(podman run --rm docker.io/outoforder/rust-builder:rocky8-$DATE rustc --version | sed 's/rustc \([[:digit:].]*\).*/\1/g')
 podman image tag docker.io/outoforder/rust-builder:rocky8-$DATE docker.io/outoforder/rust-builder:rocky8-$RUSTC_VERSION
+podman image tag docker.io/outoforder/rust-builder:rocky8-$DATE docker.io/outoforder/rust-builder:rocky8-latest
 podman image tag docker.io/outoforder/rust-builder:rocky8-$DATE docker.io/outoforder/rust-builder:latest
+
+podman image push docker.io/outoforder/rust-builder:rocky8-$DATE
+podman image push docker.io/outoforder/rust-builder:rocky8-$RUSTC_VERSION
+podman image push docker.io/outoforder/rust-builder:rocky8-latest
+podman image push docker.io/outoforder/rust-builder:latest
+
+podman build --no-cache . -f Containerfile.rocky9 -t docker.io/outoforder/rust-builder:rocky9-$DATE
+RUSTC_VERSION=$(podman run --rm docker.io/outoforder/rust-builder:rocky9-$DATE rustc --version | sed 's/rustc \([[:digit:].]*\).*/\1/g')
+podman image tag docker.io/outoforder/rust-builder:rocky9-$DATE docker.io/outoforder/rust-builder:rocky9-$RUSTC_VERSION
+podman image tag docker.io/outoforder/rust-builder:rocky9-$DATE docker.io/outoforder/rust-builder:rocky9-latest
+
+podman image push docker.io/outoforder/rust-builder:rocky9-$DATE
+podman image push docker.io/outoforder/rust-builder:rocky9-$RUSTC_VERSION
+podman image push docker.io/outoforder/rust-builder:rocky9-latest
 
