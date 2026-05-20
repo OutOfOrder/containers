f156cb78fbd8af7061fc73afaeb8a6d9e8e9c862
Edward Rudd <urkle@outoforder.cc>
2026-05-20 16:56:49 -0400

    feat: move to official rockylinux repos (under their org)

--- smtp-relay/build.sh
+++ smtp-relay/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 DATE=$(date +%Y%m%d)
-podman build . --format docker -f Dockerfile -t docker.io/outoforder/smtp-relay:$DATE
+podman build . --format docker -f Containerfile -t docker.io/outoforder/smtp-relay:$DATE
 
 podman image tag docker.io/outoforder/smtp-relay:$DATE docker.io/outoforder/smtp-relay:latest
 
