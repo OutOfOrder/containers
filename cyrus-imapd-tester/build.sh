1691b4c2adabab2fefe1122a4d54890a48641a92
Edward Rudd <urkle@outoforder.cc>
2026-06-17 12:53:02 -0400

    feat: update build script to not use ghcr.io

--- cyrus-imapd-tester/build.sh
+++ cyrus-imapd-tester/build.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 DATE=$(date +%Y%m%d)
-podman build . -f Containerfile -t ghcr.io/outoforder/cyrus-imapd-tester:$DATE-00
-podman image tag ghcr.io/outoforder/cyrus-imapd-tester:$DATE-00 ghcr.io/outoforder/cyrus-imapd-tester:latest
-podman image tag ghcr.io/outoforder/cyrus-imapd-tester:latest docker.io/outoforder/cyrus-imapd-tester:$DATE-00
-podman image tag ghcr.io/outoforder/cyrus-imapd-tester:latest docker.io/outoforder/cyrus-imapd-tester:latest
+podman build . -f Containerfile -t docker.io/outoforder/cyrus-imapd-tester:$DATE-00
+podman image tag docker.io/outoforder/cyrus-imapd-tester:$DATE-00 docker.io/outoforder/cyrus-imapd-tester:latest
 
 
